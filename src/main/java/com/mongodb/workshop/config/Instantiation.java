e90b48c8f451f0667088a2b16447df72cc8626a7
Sandro0330 <sandro030486@gmail.com>
2021-03-31 17:36:32 -0300

    Criando operação para base de dados

---
+++ src/main/java/com/mongodb/workshop/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.mongodb.workshop.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.mongodb.workshop.domain.User;
+import com.mongodb.workshop.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll(); //deleta todos os cadastros do banco
+		
+		User regina = new User(null, "Regina Castro", "regina@gmail.com");
+		User maria = new User(null, "Maria Das Dores", "maria@gmail.com");
+		User uilson = new User(null, "Uilson Alves", "alves@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(regina, maria, uilson));
+		
+	}
+
+}
