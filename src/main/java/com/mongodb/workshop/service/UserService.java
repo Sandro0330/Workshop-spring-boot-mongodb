6821a0ac73ddd8628c93f9aa94bfdcdd10d4941b
Sandro0330 <sandro030486@gmail.com>
2021-03-31 20:20:28 -0300

    Obtendo usuário por id e tratando o erro

--- src/main/java/com/mongodb/workshop/service/UserService.java
+++ src/main/java/com/mongodb/workshop/service/UserService.java
@@ -1,12 +1,14 @@
 package com.mongodb.workshop.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mongodb.workshop.domain.User;
 import com.mongodb.workshop.repository.UserRepository;
+import com.mongodb.workshop.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> objeto = userRepository.findById(id);
+		return objeto.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
