b28040fc2e1dfce183e7a06852069a4f187aef5c
Sandro0330 <sandro030486@gmail.com>
2021-04-03 18:36:50 -0300

    Projeção dos dados do autor com o padrão DTO

--- src/main/java/com/mongodb/workshop/config/Instantiation.java
+++ src/main/java/com/mongodb/workshop/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.mongodb.workshop.domain.Post;
 import com.mongodb.workshop.domain.User;
+import com.mongodb.workshop.dto.AuthorDTO;
 import com.mongodb.workshop.repository.PostRepository;
 import com.mongodb.workshop.repository.UserRepository;
 
@@ -28,18 +29,19 @@ public class Instantiation implements CommandLineRunner {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		userRepository.deleteAll(); //deleta todos os cadastros do banco
+		userRepository.deleteAll(); //deleta todos os cadastros do banco 
 		postRepository.deleteAll();
 		
 		User regina = new User(null, "Regina Castro", "regina@gmail.com");
 		User maria = new User(null, "Maria Das Dores", "maria@gmail.com");
 		User uilson = new User(null, "Uilson Alves", "alves@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(regina, maria, uilson));
 		
-		Post post1 = new Post(null, simpleDateFormat.parse("21/03/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, simpleDateFormat.parse("22/03/2021"), "Bom Dia", "Espero que todos tenham um bom dia!", maria);
+		Post post1 = new Post(null, simpleDateFormat.parse("21/03/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, simpleDateFormat.parse("22/03/2021"), "Bom Dia", "Espero que todos tenham um bom dia!", new AuthorDTO(maria));
 	 
 		
-		userRepository.saveAll(Arrays.asList(regina, maria, uilson));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
